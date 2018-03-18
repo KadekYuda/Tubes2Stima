b78b7d2a739a418800cbd121e6ef14a8d7d2c4fa
Jonathan <yokanaro@gmail.com>
2018-03-18 17:37:09 +0700

    Added static method to create dependency matrix

---
+++ Tubes2/Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes2
+{
+    class Tree
+    {
+        public static List<List<int>> createDependencyMatrix(List<List<String>> graphdetail)
+        {
+            //Sort by class code
+            graphdetail = graphdetail.OrderBy(x => x[0]).ToList();
+
+            List<List<int>> dependency_matrix = new List<List<int>>();
+
+            //Initialize matrix with 
+            for(int i = 0; i < graphdetail.Count; i++)
+            {
+                dependency_matrix.Add(new List<int>(new int[graphdetail.Count]));
+            }
+
+            for(int i = 0; i < graphdetail.Count;i++)
+            {
+                foreach(String dependency in graphdetail[i].Skip(1))
+                {
+                    int j = graphdetail.FindIndex(x => (x[0] == dependency));
+                    dependency_matrix[j][i] = 1;
+                }
+            }
+
+            return dependency_matrix;
+        }
+    }
+}
