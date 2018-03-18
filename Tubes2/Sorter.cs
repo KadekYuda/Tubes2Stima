ba839e7f3cd04e88f29b0a37c7dac887f4513ff5
Jonathan <yokanaro@gmail.com>
2018-03-18 20:02:45 +0700

    Added dependent matrix creation and BFSSorter

---
+++ Tubes2/Sorter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes2
+{
+    class BFSSorter
+    {
+        public static Queue<String> topoSortBFS(List<List<String>> graphdetail)
+        {
+            List<List<bool>> dependent_matrix = Tree.createDependentMatrix(graphdetail);
+
+            Queue<String> ordered_graph = new Queue<String>();
+
+            while(dependent_matrix.Exists(x => x.Contains(true)))
+            {
+                for(int i = 0; i < dependent_matrix.Count; i++)
+                {
+                    if(!dependent_matrix[i].Contains(true))
+                    {
+                        ordered_graph.Enqueue(graphdetail[i][0]);
+                        for(int j = 0; j < dependent_matrix.Count; j++)
+                        {
+                            dependent_matrix[j][i] = false;
+                        }
+                    }
+                }
+            }
+
+            return ordered_graph;
+        }
+    }
+}
--- Tubes2/Tree.cs
+++ Tubes2/Tree.cs
@@ -8,25 +8,46 @@ namespace Tubes2
 {
     class Tree
     {
-        public static List<List<int>> createDependencyMatrix(List<List<String>> graphdetail)
+        public static List<List<bool>> createDependencyMatrix(List<List<String>> graphdetail)
         {
-            //Sort by class code
-            graphdetail = graphdetail.OrderBy(x => x[0]).ToList();
-
-            List<List<int>> dependency_matrix = new List<List<int>>();
+            List<List<bool>> dependency_matrix = new List<List<bool>>();
 
             //Initialize matrix with 
             for(int i = 0; i < graphdetail.Count; i++)
             {
-                dependency_matrix.Add(new List<int>(new int[graphdetail.Count]));
+                dependency_matrix.Add(new List<bool>(new bool[graphdetail.Count]));
             }
 
+            //Fill in the matrix
             for(int i = 0; i < graphdetail.Count;i++)
             {
                 foreach(String dependency in graphdetail[i].Skip(1))
                 {
                     int j = graphdetail.FindIndex(x => (x[0] == dependency));
                     dependency_matrix[j][i] = 1;
+                }
+            }
+
+            return dependency_matrix;
+        }
+
+        public static List<List<bool>> createDependentMatrix(List<List<String>> graphdetail)
+        {
+            List<List<bool>> dependency_matrix = new List<List<bool>>();
+
+            //Initialize matrix with 
+            for (int i = 0; i < graphdetail.Count; i++)
+            {
+                dependency_matrix.Add(new List<bool>(new bool[graphdetail.Count]));
+            }
+
+            //Fill in the matrix
+            for (int i = 0; i < graphdetail.Count; i++)
+            {
+                foreach (String dependency in graphdetail[i].Skip(1))
+                {
+                    int j = graphdetail.FindIndex(x => (x[0] == dependency));
+                    dependency_matrix[i][j] = 1;
                 }
             }
 
